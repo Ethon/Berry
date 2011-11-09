1375da03bca9feaaaa60a0daa11760e43b2550d9
Ethon <ethon@ethon.cc>
2011-11-09 11:50:01 +0100

    Added tests to test process list iteration.

--- test/test_processes.cpp
+++ test/test_processes.cpp
@@ -11,6 +11,8 @@
 
 // Berry:
 #include <berry/process.hpp>
+#include <berry/process_entry.hpp>
+#include <berry/process_iterator.hpp>
 
 using berry::process;
 
@@ -76,6 +78,49 @@ BOOST_AUTO_TEST_CASE(BerryProcessGetBitness)
       
    std::cout << "Current process bitness: " << bitness << '\n';
    BOOST_CHECK_EQUAL(bitness, real_bitness);
+}
+
+// Test berry::get_entry_by_pid.
+BOOST_AUTO_TEST_CASE(BerryGetEntryByPid)
+{
+   process self(berry::get_current_process());
+   
+   boost::optional<berry::process_entry> by_pid(
+      berry::get_entry_by_pid(self.get_pid()));
+   BOOST_REQUIRE(by_pid);
+   BOOST_CHECK_EQUAL(berry::get_name(self), by_pid->name);
+}
+
+// Test berry::get_entry_by_name 1
+BOOST_AUTO_TEST_CASE(BerryGetEntryByName1)
+{
+   process self(berry::get_current_process());
+   std::string self_name(berry::get_name(self));
+   
+   boost::optional<berry::process_entry> by_name(
+      berry::get_entry_by_name(self_name));
+   BOOST_REQUIRE(by_name);
+   BOOST_CHECK_EQUAL(self_name, by_name->name);
+}
+
+// Test berry::get_entry_by_name 2
+BOOST_AUTO_TEST_CASE(BerryGetEntryByName2)
+{
+   process self(berry::get_current_process());
+   std::string self_name(berry::get_name(self));
+   
+   boost::optional<berry::process_entry> by_name1(
+      berry::get_entry_by_name(self_name, true));
+   BOOST_REQUIRE(by_name1);
+   
+   std::string upper_case_name(self_name);
+   std::transform(self_name.begin(), self_name.end(),
+      upper_case_name.begin(), &toupper);
+   boost::optional<berry::process_entry> by_name2(
+      berry::get_entry_by_name(self_name, false));
+   BOOST_REQUIRE(by_name2);  
+   
+   BOOST_CHECK_EQUAL(by_name1->pid, by_name2->pid);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
