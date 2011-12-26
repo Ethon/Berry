b299aadada01cb1c8034fa35626f9520eea48ac0
Florian Erler <ethon@ethon.cc>
2011-12-26 17:34:52 +0100

    Changed almost everything in the public process API, dumped get_working_dir (probably for ever) and simple_create_process (will be written again, but with a different design). I swear this will be stable from now on...

--- include/berry/process.hpp
+++ include/berry/process.hpp
@@ -37,123 +37,138 @@
 
 namespace berry
 {
-   /**
-   * @brief Represents a process on the system.
-   */
-   class process
-   {
-   public:
-      /**
-      * @brief Defines a type used to store arbitrary process identifiers.
-      */
-      typedef detail::process::pid_type pid_type;
-      
-   private:
-      pid_type m_pid;
-      
-   public:
-      /**
-       * @brief Constructs a process object which doesn't represent a process.
-       */
-      process();
+    /**
+     * @brief Defines a type used to store arbitrary process identifiers.
+     **/
+    typedef detail::process::pid_type pid_type;
+        
+    /**
+     * @brief Represents a process on the system.
+     **/
+    class process : private detail::process::process_data
+    {
+    public:
+        /**
+         * @brief Constructs a process object which doesn't represent a process.
+         **/
+        process();
     
-      /**
-       * @brief Constructs a process object from a process id.
-       * 
-       * @param pid The target pid.
-       */
-      explicit process(pid_type pid);
-      
-      /**
-       * @brief Returns the pid of the process.
-       *
-       * @return pid_type The pid of the process.
-       */
-      pid_type get_pid() const;
-   };
-   
-   /**
-    * @brief Returns the name of the specified process.
-    * Many systems limit this to a few characters, so it may be incomplete
-    * on those systems. Use get_executable_path to be sure to determine the
-    * full name.
-    * @param proc The target process.
-    * @return :string& The process' name
-    * @see get_executable_path
-    */
-   std::string get_name(process proc);
-    
-   /**
-    * @brief Makes simple checks to determine if the process still exists.
-    *
-    * @param proc The target process.
-    * @return bool True if the process still exists, false otherwise.
-    */
-   bool still_exists(process proc);
-    
-   namespace unix_like
-   {
-      /**
-       * @brief Returns the ProcFS directory of the specified process.
-       * Some systems implement the Proc filesystem. For those systems, the
-       * path to the process' directory inside the ProcFS is returned.
-       * @param proc The target process.
-       * @return :filesystem3::path The path to the ProcFS directory or an
-       * empty path, if not implemented.
-       */
-      boost::filesystem::path get_procfs_dir(process proc);
-   }
-    
-   /**
-    * @brief Returns the working directory of the specified process.
-    *
-    * @param proc The target process.
-    * @return :filesystem3::path The path to the working directory.
-    */
-   boost::filesystem::path get_working_dir(process proc);
-    
-   /**
-   * @brief Returns the path to the binary file of the specified process.
-   * On some systems you need to have administrative rights to query
-   * this value from processes run by other users.
-   * @param proc The target process.
-   * @return :filesystem3::path The path to the binary file.
-   */
-   boost::filesystem::path get_executable_path(process proc);
+        /**
+         * @brief Constructs a process object from a process id.
+         * 
+         * @param pid The process' id.
+         **/
+        explicit process(pid_type pid);
+        
+        /**
+         * @brief Constructs a process object from a process name.
+         *
+         * @param name The process' name.
+         * @param case_sensitive Pass true to search case sensitive.
+         **/
+        process(std::string const& name, bool case_sensitive = true);
+        
+        /**
+         * @brief Constructs a process object from copying another process.
+         * 
+         * @param other Another process to copy.
+         **/
+        process(process const& other);
+        
+        /**
+         * @brief Constructs a process object from moving another process.
+         * 
+         * @param other Another process to move.
+         **/
+        process(process&& other);
+        
+        /**
+         * @brief Destructs the process object.
+         **/
+        ~process();
+        
+        /**
+         * @brief Copies another process object.
+         *
+         * @param other Another process to copy.
+         * @return :process& *this
+         **/
+        process& operator=(process const& other);
+        
+        /**
+         * @brief Moves another process object.
+         * 
+         * @param other Another process to move.
+         * @return :process& *this
+         **/
+        process& operator=(process&& other);
 
-   /**
-   * @brief Returns the bitness of the specified process (32/64 bit)
-   *
-   * @param proc The target process.
-   * @return int The result as integer, 0 if the value cant be determined.
-   */
-   int get_bitness(process proc);
+        /**
+         * @brief Returns the process' pid.
+         *
+         * @return :pid_type The process' pid.
+         **/
+        pid_type pid() const;
+
+        /**
+         * @brief Retrieves the process' name.
+         * Many systems limit this to a few characters, so it may be incomplete
+         * on those systems. Use executable_path to be sure to determine the
+         * full name.
+         * @return :string The process' namespace
+         * @see executable_path
+         **/
+        std::string name() const;
+        
+        /**
+         * @brief Retrieves the path to the process' executable file.
+         *
+         * @return :filesystem3::path The path to the binary file.
+         **/
+        boost::filesystem::path executable_path() const;
+        
+        /**
+         * @brief Retrieves the process' bitness. (32/64 bit)
+         *
+         * @return int The result as integer.
+         **/
+        int bitness() const;
+
+        /**
+         * @brief Terminates the process. 
+         *
+         * @param force Pass true to force the process' termination.
+         **/
+        void terminate(bool force = false);
+        
+        /**
+         * @brief Makes simple checks to determine if the process still exists.
+         *
+         * @return bool True if the process still exists, false otherwise.
+         **/
+        bool still_exists(process proc);
+    };
     
-   /**
-   * @brief Returns the currently active process calling this function.
-   *
-   * @return :process& The current process.
-   */
-   process get_current_process();
+    /**
+     * @brief Returns the currently active process calling this function.
+     *
+     * @return :process& The current process.
+     **/
+    process const& get_current_process();
     
-   /**
-   * @brief Creates a new process which runs as the current process' child.
-   * One some systems (like Linux) this is an asynchronous action, so don't
-   * expect the new process to be running right after this function returns.
-   * @param arguments The arguments to the process where the command to run
-   * is passed as the first argument.
-   * @return :process The created child process.
-   */
-   process simple_create_process(std::vector<std::string> const& arguments);
-  
-   /**
-    * @brief Terminates the specified process. 
-    * Some systems allow to pass a exit code which is returned as the process'
-    * exit code, other systems will ignore this parameter.
-    * @param proc The target process.
-    * @param exit_code An optional exit code value.
-    */
-   void terminate_process(process proc, int exit_code = 0);
+#ifdef BERRY_HAS_PROCFS 
+    namespace unix_like
+    {
+        /**
+         * @brief Returns the ProcFS directory of the specified process.
+         * Some systems implement the Proc filesystem. For those systems, the
+         * path to the process' directory inside the ProcFS is returned.
+         * @param proc The target process.
+         * @return :filesystem3::path The path to the ProcFS directory.
+         **/
+        boost::filesystem::path get_procfs_dir(process proc);
+    }
+#endif
 }
 
 #endif // __BERRY_PROCESS_HPP__
